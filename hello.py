ac66cb2838d8bfd6ea7f26b1541d9e2d00850fb9
Deepanjan Roy <deepanjan.roy@mail.mcgill.ca>
2014-03-29 09:02:05 -0400

    (hopefully successful) static test site

--- hello.py
+++ hello.py
@@ -6,15 +6,24 @@ db = client.testdb1
 col = db.testData
 
 urls = (
-    '/', 'index'
+    '/', 'index',
+    '/test', 'test'
 )
 
-
+class test:
+    def GET(self):
+        render = web.template.render('static/test')
+        return render.fbauth()
 
 class index:
+    # def GET(self):
+    # 	database_stuff = list(col.find());
+    #     return str(database_stuff);
+
     def GET(self):
-	database_stuff = list(col.find());
-        return str(database_stuff);
+        render = web.template.render('templates')
+        return render.index()
+
 
 # For serving WSGI
 wsgi_app = web.application(urls, globals()).wsgifunc()
