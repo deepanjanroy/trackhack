f4160a1adb451fb04d21f0df81baa191f37ad529
Deepanjan Roy <deepanjan.roy@mail.mcgill.ca>
2014-03-29 21:49:09 -0400

    Facebook login works

--- static/semantic/examples/homepage.js
+++ static/semantic/examples/homepage.js
@@ -53,5 +53,19 @@ $(document)
 
     setInterval(changeSides, 3000);
 
+    try_login = function () {
+      if(window.facebook_setup_complete && window.github_setup_complete){
+        success_data = {
+          "facebook_id" : window['fb_user_id'],
+          "github_username": "foobar" // NEEEEEDS TO BE FIOXED SDFNSDLFJSDLAFKASDLFKSADFLNASDJFL SDAFK
+        }
+        // jQuery.post( url [, data ] [, success(data, textStatus, jqXHR) ] [, dataType ] )
+        $.post("/login_success", success_data, function (data, textStatus, jqXHR) {
+          window.location.replace("/userhome");
+        });
+      }
+
+    }
+
   })
 ;
