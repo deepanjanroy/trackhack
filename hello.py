0069f251cf6104f0cb00dec2fd755d916eb7c254
Sami Jaber <jabersami@gmail.com>
2014-03-29 21:53:33 -0400

    added github button to main site

--- hello.py
+++ hello.py
@@ -5,8 +5,10 @@ client = MongoClient(os.environ.get("TH_MONGO_URL"));
 db = client.testdb1
 col = db.testData
 
+github_client_id = MongoClient(os.environ.get("GITHUB_CLIENT_ID"))
+github_secret_id = MongoClient(os.environ.get("GITHUB_SECRET_ID"))
+
 urls = (
-    '/', 'index',
     '/test', 'test',
 	'/enter', 'authenticate'
 )
@@ -27,6 +29,26 @@ class authenticate:
 		render = web.template.render('templates/')
 		return render.enter()
 		#wow wooooow so intense
+
+class logged:
+	def GET(self):
+		user_data = web.input()
+		url = urllib2.urlopen("https://github.com/login/oauth/access_token", "scope=user,repo&client_id=" + github_client_id + "&client_secret=" + github_secret_id + "&code=" + user_data.code)		
+		resp = url.read().replace('=', '&').split('&')
+		access_token = resp[1]
+		render = web.template.render('.')
+		return render.repo(access_token)
+
+class create:
+	def GET(self):
+		user_data = web.input()
+		repoName = json.dumps({"name": user_data.name})
+		
+		req = urllib2.Request("https://api.github.com/user/repos?access_token=" + user_data.access_token, repoName, {'Content-Type': 'application/json'})
+		f = urllib2.urlopen(req)
+		response = f.read()
+		f.close()
+		return "HAHAHA"
 
 
 # For serving WSGI
